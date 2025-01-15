aaf07711c649dcc734d3ec823e3dccd2b18e5ad5
gunslord <gunslord@gmail.com>
2025-01-15 23:44:45 +0900

    no message

---
+++ day10/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+const Welcome = ({ name, isMember }) => {
+  return (
+    <div>
+      {isMember ? `${name}님 다시 오셨군요` : `${name}님 가입하시겠어요?`}
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Welcome name={"Kevin"} isMember={false}></Welcome>
+    </>
+  );
+}
+
+export default App;
