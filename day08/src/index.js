c4494adc12b58fc5ea0e94e12c20310b86e814c0
gunslord <gunslord@gmail.com>
2025-01-16 00:00:41 +0900

    no message

---
+++ day08/src/index.js
@@ -0,0 +1,16 @@
+import { getRandomName, getRandomEmail, getRandomCompany } from "./random.js";
+
+const name1 = getRandomName();
+const name2 = getRandomName();
+const name3 = getRandomName();
+console.log(name1, name2, name3);
+
+const email1 = getRandomEmail();
+const email2 = getRandomEmail();
+const email3 = getRandomEmail();
+console.log(email1, email2, email3);
+
+const company1 = getRandomCompany();
+const company2 = getRandomCompany();
+const company3 = getRandomCompany();
+console.log(company1, company2, company3);
